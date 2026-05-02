16a9596695499e906355238b125a85d724a01608
Chhuy-bit <trachet297@gmail.com>
2026-05-02 14:40:45 +0700

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <footer className='bg-black py-4 text-white lg:text-xl md:text-[11px] text-[7px] mt-10'>
+    <footer className='bg-black py-4 text-white lg:text-md md:text-[11px] text-[7px] mt-10'>
         <div className='max-w-[90%] lg:max-w-5xl mx-auto flex flex-row justify-between items-center'>
             <h4>Copyright &#169;; -MasterIT Shop</h4>
             <ul className="flex items-center lg:gap-8 gap-3">

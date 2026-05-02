343956a7757155c3f79e1212ff346fdfbac87670
Chhuy-bit <trachet297@gmail.com>
2026-05-02 07:56:18 +0700

    edit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import TopBar from "./components/TopBar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import TopBar from "../components/TopBar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function RootLayout() {
   return (
